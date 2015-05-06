b455285ce57e39eeabe9ede3a5b313a516a57549
ami-GS <1991.daiki@gmail.com>
2015-05-06 14:23:34 +0900

    implement handshake validation method

--- client.go
+++ client.go
@@ -1,8 +1,9 @@
 package gows
 
 import (
-	//"fmt"
+	"fmt"
 	"net"
+	"strings"
 )
 
 type Client struct {
@@ -26,6 +27,28 @@ func (self *Client) Ping(data string) {
 	self.connections[0].Send([]byte(data), PING)
 }
 
+// TODO: gather validate function to 1 (client and server)
+func (self *Client) ValidateResponse(buffer []byte) (validate bool) {
+	af := strings.Split(string(buffer), "\n")
+	validate = true
+	for _, v := range af {
+		if strings.Contains(v, "HTTP/1.1") {
+			continue
+		}
+
+		header := strings.Split(v, ": ")
+		fmt.Printf("%v ", header)
+		if header[0] == "Upgrade" && header[1] != "websocket" {
+			validate = false
+		} else if header[0] == "Connection" && header[1] != "Upgrade" {
+			validate = false
+		} else if header[0] == "Sec-Websocket-Accept" && header[1] != "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=" {
+			validate = false
+		}
+	}
+	return
+}
+
 func (self *Client) Connect(addr string) {
 	for _, con := range self.connections {
 		if addr == con.addr {
@@ -34,7 +57,11 @@ func (self *Client) Connect(addr string) {
 	}
 	conn, _ := net.Dial("tcp", addr)
 	connection := NewConnection(&conn, addr)
-	if connection.ValidateHandshake() {
+	connection.SendHandshake()
+	buffer, _ := connection.Read(256)
+	if !self.ValidateResponse(buffer) {
+		connection.Close()
+	} else {
 		self.connections = append(self.connections, *connection)
 		go connection.ReceiveLoop()
 	}
--- server.go
+++ server.go
@@ -3,12 +3,14 @@ package gows
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 type Server struct {
 	clients map[string]*Connection
 	serv    *net.Listener
 	addr    string
+	version string
 }
 
 func NewServer(addr string) *Server {
@@ -18,9 +20,35 @@ func NewServer(addr string) *Server {
 	}
 	server := &Server{
 		map[string]*Connection{"": &Connection{}},
-		&serv, addr}
+		&serv, addr, "13"}
 
 	return server
+}
+
+// TODO: gather validate function to 1 (client and server)
+func (self *Server) ValidateRequest(buffer []byte) (validate bool) {
+	af := strings.Split(string(buffer), "\n")
+	validate = true
+	for _, v := range af {
+		if strings.Contains(v, "HTTP/1.1") {
+			continue
+		}
+		header := strings.Split(v, ": ")
+		fmt.Printf("%v ", header)
+		if header[0] == "Upgrade" && header[1] != "websocket" {
+			validate = false
+		} else if header[0] == "Connection" && header[1] != "Upgrade" {
+			validate = false
+		} else if header[0] == "Sec-Websocket-Key" && header[1] != "dGhlIHNhbXBsZSBub25jZQ==" {
+			validate = false
+		} else if header[0] == "Sec-WebSocket-Protocol" && !strings.Contains(header[1], "chat") {
+			validate = false
+		} else if header[0] == "Sec-WebScoket-Version" && !strings.Contains(header[1], self.version) {
+			validate = false
+		}
+
+	}
+	return
 }
 
 func (self *Server) WaitClient() {
@@ -32,12 +60,12 @@ func (self *Server) WaitClient() {
 
 		connection := NewConnection(&conn, conn.LocalAddr().String())
 		self.clients[conn.LocalAddr().String()] = connection
-		buffer, err := connection.Read(1024)
-		if err != nil {
-			//
+		buffer, _ := connection.Read(1024)
+		if !self.ValidateRequest(buffer) {
+			connection.Close()
+		} else {
+			connection.ResponseOpeningHandshake()
+			go connection.ReceiveLoop()
 		}
-		fmt.Printf("%s\n", buffer)
-		connection.ResponseOpeningHandshake()
-		go connection.ReceiveLoop()
 	}
 }
